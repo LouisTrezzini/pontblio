d7b1c4cc7d158bb6c2cd95e3d7e3b25dd20738a9
Louis Trezzini <till034@gmail.com>
2015-12-23 17:26:32 +0100

    Utils

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,19 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
+use JWTAuth;
+
+use Exception;
+
 abstract class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @return \App\User $user
+     */
+    public function getAuthUser()
+    {
+        return JWTAuth::parseToken()->authenticate();
+    }
 }
