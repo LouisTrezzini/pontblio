861613f77ed5089f5e47ad46bc83806213caf254
Louis Trezzini <till034@gmail.com>
2015-12-23 16:01:21 +0100

    Removing Entrust

--- app/User.php
+++ app/User.php
@@ -6,12 +6,9 @@ use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-use Zizaco\Entrust\Traits\EntrustUserTrait;
-
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
 
-    use EntrustUserTrait;
     use Authenticatable, CanResetPassword;
     /**
      * The database table used by the model.
