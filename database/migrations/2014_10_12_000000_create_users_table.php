fd75d2e0d8298e79da9c4ca048a5ef6bbe25c3f3
Louis Trezzini <till034@gmail.com>
2015-12-24 16:05:43 +0100

    Nullable columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password', 60);
 
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
 
             $table->integer('role_id');
 
