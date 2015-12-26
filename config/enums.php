07e237a0f7815583fec08728398ff1e3bb7c6ff9
Louis Trezzini <till034@gmail.com>
2015-12-26 01:28:41 +0100

    Enums update

--- config/enums.php
+++ config/enums.php
@@ -10,4 +10,41 @@ return [
         'soutenance' => 'Exposé / Soutenance',
         'skype' => 'Entretien via Skype',
     ],
+    'user_profile' => [
+        'user_profile_aue' => 'AUE - Architecte urbaniste de l\'état',
+        'user_profile_inge' => 'Elève ingénieur',
+        'user_profile_master' => 'Elève en master',
+        'user_profile_master_spec' => 'Elève en master spécialisé',
+        'user_profile_other' => 'Autre'
+    ],
+    'user_profile_aue' => [],
+    'user_profile_inge' => [
+        '1a' => 'Formation d\'ingénieur - 1ère année',
+        '2a' => 'Formation d\'ingénieur - 2ème année',
+        '3a' => 'Formation d\'ingénieur - 3ème année',
+    ],
+    'user_profile_master' => [
+        'anedp' => 'Master ANEDP - Analyse numérique et équations aux dérivés partielles',
+        'dmse' => 'Master DMSE - Durabilité des matériaux et des structures pour l\'énergie',
+        'eddee' => 'Master EDDEE - Economie de l\'environnement, du développement durable et de l\'énergie',
+        'eis' => 'Master EIS - Entreprise, innovation, société',
+        'en' => 'Master EN - Énergie Nucléaire, spécialité démantèlement et gestion des déchets',
+        'fipro' => 'Master FIPRO Financement de projet - Financement structuré',
+        'gtsed' => 'Master GTESD - Gestion et traitement des eaux',
+        'maf' => 'Master MAF - Mathématiques appliqués en finance',
+    ],
+    'user_profile_master_spec' => [
+
+    ],
+    'user_profile_other' => [
+
+    ],
+    'dept' => [
+        'gcc' => 'GCC',
+        'gi' => 'GI',
+        'gmm' => 'GMM',
+        'segf' => 'SEGF',
+        'imi' => 'IMI',
+        'vet' => 'VET'
+    ]
 ];
