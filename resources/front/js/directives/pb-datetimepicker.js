b2769d58f92abbbdb2efba36d5b5dae427a84dc1
Louis Trezzini <till034@gmail.com>
2016-01-03 18:38:58 +0100

    Adding pbDatetimepicker directive

---
+++ resources/front/js/directives/pb-datetimepicker.js
@@ -0,0 +1,32 @@
+angular.module('biblio').directive('pbDatetimepicker', function () {
+    return {
+        scope: {
+            result: '='
+        },
+        controller: ['$scope', '$resource', function($scope, $resource) {
+            $scope.update = function(){
+                $scope.result = moment($scope.date)
+                    .hours($scope.hours)
+                    .minutes($scope.minutes)
+                    .toDate();
+
+                console.log($scope.result);
+            };
+
+            if($scope.result) {
+                $scope.date = moment($scope.result).toDate();
+                $scope.hours = moment($scope.result).hours();
+                $scope.minutes = moment($scope.result).minutes();
+            }
+
+            $scope.$watch('hours', function(value) {
+                $scope.update();
+            });
+
+            $scope.$watch('minutes', function(value) {
+                $scope.update();
+            });
+        }],
+        templateUrl: 'views/pb-datetimepicker.html'
+    };
+});
