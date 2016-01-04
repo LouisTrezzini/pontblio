85f98716f105aca5e10a0283654c7cf2b395b270
Louis Trezzini <till034@gmail.com>
2016-01-04 11:55:19 +0100

    Updating enums

--- config/enums.php
+++ config/enums.php
@@ -11,19 +11,27 @@ return [
         'skype' => 'Entretien via Skype',
     ],
     'user_profile' => [
+        'user_profile_not_selected' => 'Non sélectionné',
         'user_profile_aue' => 'AUE - Architecte urbaniste de l\'état',
         'user_profile_inge' => 'Elève ingénieur',
         'user_profile_master' => 'Elève en master',
         'user_profile_master_spec' => 'Elève en master spécialisé',
         'user_profile_other' => 'Autre'
     ],
-    'user_profile_aue' => [],
+    'user_profile_not_selected' => ['
+        not_selected' => 'Non sélectionné'
+    ],
+    'user_profile_aue' => [
+        'not_selected' => 'Non sélectionné'
+    ],
     'user_profile_inge' => [
+        'not_selected' => 'Non sélectionné',
         '1a' => 'Formation d\'ingénieur - 1ère année',
         '2a' => 'Formation d\'ingénieur - 2ème année',
         '3a' => 'Formation d\'ingénieur - 3ème année',
     ],
     'user_profile_master' => [
+        'not_selected' => 'Non sélectionné',
         'anedp' => 'Master ANEDP - Analyse numérique et équations aux dérivés partielles',
         'dmse' => 'Master DMSE - Durabilité des matériaux et des structures pour l\'énergie',
         'eddee' => 'Master EDDEE - Economie de l\'environnement, du développement durable et de l\'énergie',
@@ -34,10 +42,10 @@ return [
         'maf' => 'Master MAF - Mathématiques appliqués en finance',
     ],
     'user_profile_master_spec' => [
-
+        'not_selected' => 'Non sélectionné'
     ],
     'user_profile_other' => [
-
+        'not_selected' => 'Non sélectionné'
     ],
     'departement' => [
         'gcc' => 'GCC',
