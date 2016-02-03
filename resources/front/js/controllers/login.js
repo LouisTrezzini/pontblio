82a547e83d1ce34f17cae54247fed0823cd770e4
Louis Trezzini <louis.trezzini@eleves.enpc.fr>
2016-02-03 09:45:30 +0100

    Invalid credentials error message

--- resources/front/js/controllers/login.js
+++ resources/front/js/controllers/login.js
@@ -1,5 +1,5 @@
 angular.module('biblio')
-    .controller('Login_Ctrl', function($scope, $rootScope, $auth, $state) {
+    .controller('Login_Ctrl', function($scope, $rootScope, $auth, $state, Alert) {
         $scope.login = function(username, password) {
             var credentials = {
                 username: username,
@@ -7,7 +7,7 @@ angular.module('biblio')
             };
 
             // Use Satellizer's $auth service to login
-            $auth.login(credentials).then(function (data) {
+            $auth.login(credentials).then(function (response) {
                 var payload = $auth.getPayload();
                 $rootScope.isAdmin = payload.role == 'gestion';
                 $rootScope.isModo = $rootScope.isAdmin || payload.role == 'biblio';
@@ -15,6 +15,11 @@ angular.module('biblio')
 
                 // If login is successful, redirect to the users state
                 $state.go('root.home');
+            }, function(response) {
+                if(response.data.error == 'invalid_credentials')
+                    Alert.toast('Identifiants incorrects');
+                else
+                    Alert.toast('Une erreur est survenue');
             });
         };
     })
